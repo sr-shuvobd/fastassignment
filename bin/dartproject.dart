ddda1f18f09a04d9229f8f1cc43d04a14ecac8d3
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2023-05-02 15:55:24 +0600

    my fastassignment in ostad modiul 1

---
+++ bin/dartproject.dart
@@ -0,0 +1,13 @@
+void main(){
+
+
+  List number= ['+88','01705867949','01319151516','01319151516','01828842883',
+  ];
+  print(number[0] + number[1]);
+  print(number[0] + number[2]);
+  print(number[0] + number[3]);
+  print(number[0] + number[4]);
+
+}
+
+
